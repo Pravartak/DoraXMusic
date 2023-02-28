cc514c33ef9db27064a94007922a4bca84e113d9
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-02-28 20:03:47 +0530

    Update callbot.py

--- callbot.py
+++ callbot.py
@@ -1,21 +1,17 @@
-import pyrogram
-from pyrogram import Client, filters
+from TgCallBot import config
+from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls import idle
 from pytgcalls.types import AudioPiped
+...
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+app = PyTgCalls(client)
 
-api_id = 16519771
-api_hash ="83754b17b54fe9d6a602e70c458afddb"
-chat_id = -1001600386203
-    
-async def main():
-    async with Client("my_account", api_id, api_hash) as bot:
-      app = PyTgCalls(bot)
 app.start()
 app.join_group_call(
-chat_id,
-  AudioPiped(
-      'http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4',
-   )
-)
+        -1001796731397,
+        AudioPiped(
+            'http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4',
+        )
+    )
 idle()
