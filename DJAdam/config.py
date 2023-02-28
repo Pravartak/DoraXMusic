00777db15c805cd23f5d10b806f3b26365d9f965
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-02-28 20:40:03 +0530

    Update config.py

--- DJAdam/config.py
+++ DJAdam/config.py
@@ -2,6 +2,7 @@ from os import getenv
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
 admins = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
