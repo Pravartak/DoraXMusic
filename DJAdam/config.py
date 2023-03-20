722bd9ff9607d20fc8a26d217a7246f9ba9c6055
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-03-20 16:19:47 +0530

    Delete config.py

--- DJAdam/config.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-admins = {}
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
