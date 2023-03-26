1a018612a882f0e60b2c81c3b89ab9b72fa86071
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-03-26 20:20:20 +0530

    Create language.py

---
+++ language.py
@@ -0,0 +1,44 @@
+from __init__ import get_string
+from misc import SUDOERS
+from database import (get_lang, is_commanddelete_on,
+                                       is_maintenance)
+
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    "Bot is under maintenance. Please wait for some time..."
+                )
+        if await is_commanddelete_on(message.chat.id):
+            try:
+                await message.delete()
+            except:
+                pass
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    "Bot is under maintenance. Please wait for some time...",
+                    show_alert=True,
+                )
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
