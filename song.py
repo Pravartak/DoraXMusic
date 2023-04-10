be66e84764590c30b34d422fe852797f1a2e9f86
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-04-10 09:20:35 +0530

    Update song.py

--- song.py
+++ song.py
@@ -1,21 +1,21 @@
 from pyrogram.types import InlineKeyboardButton
 
 
-def song_markup(_, vidid):
+def song_markup(vidid):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["SG_B_2"],
+                text="SG_B_2",
                 callback_data=f"song_helper audio|{vidid}",
             ),
             InlineKeyboardButton(
-                text=_["SG_B_3"],
+                text="SG_B_3",
                 callback_data=f"song_helper video|{vidid}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close"
+                text="CLOSE_BUTTON", callback_data="close"
             ),
         ],
     ]
