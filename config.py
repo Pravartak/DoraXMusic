40194552eeaf12efec382530fbba977cc80a09dc
Pravartak <109897902+Pravartak@users.noreply.github.com>
2023-10-24 12:09:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,8 @@ import sys
 from dotenv import load_dotenv
 from pyrogram import filters
 
-load_dotenv()
-
-API_ID = int("16519771")
+API_ID = 16519771
 API_HASH = "83754b17b54fe9d6a602e70c458afddb"
 BOT_TOKEN = "6165241065:AAFzBHV-9lz4-BICl_iw07Bk4k5jOKK8i-c"
-LOG_GROUP_ID = int("-1001796731397")
+LOG_GROUP_ID = -1001796731397
 SET_CMD = True
